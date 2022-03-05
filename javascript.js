128d8dc6b05ff6cdc1beb624f6a256373f4820e0
ecoronado <lincicoronado@gmail.com>
2022-03-04 21:51:17 -0500

    more javascript exercises

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function favoriteAnimal(animal) {
+    console.log(animal + " is my favorite animal!")
+}
+
+favoriteAnimal('Cat')
+
+function ask(question, yes, no) {
+    if (confirm(question)) yes ();
+    else no()
+}
+
+ask(
+    "Do you agree?",
+    () => alert("You agreed."),
+    () => alert("You canceled the execution.")
+);
